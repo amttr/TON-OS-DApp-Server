a89db7bca7d032da1468baddc215994f5281cd22
Dima <9569473+amttr@users.noreply.github.com>
2020-09-03 11:00:00 +0300

    fix: minor issues with dedicated inventory builder

--- infra/dedicated/build_inventory.sh
+++ infra/dedicated/build_inventory.sh
@@ -5,10 +5,10 @@ TONOS_USER=$2
 GIT_SSH_ID_PATH=$3
 
 INVENTORY=../../ansible/inventory
-INVENTORY_TEMPLATE=../inventory.tpl
+INVENTORY_TEMPLATE=../templates/inventory.tpl
 
 cp -a $INVENTORY_TEMPLATE $INVENTORY
 
-sed -i "s/\${tonos_ip}/$TONOS_IP/" $INVENTORY
-sed -i "s/\${tonos_user}/$TONOS_USER/" $INVENTORY
-sed -i "s/\${git_ssh_id_path}/$GIT_SSH_ID_PATH/" $INVENTORY
+sed -i "s|\${tonos_ip}|$TONOS_IP|" $INVENTORY
+sed -i "s|\${tonos_user}|$TONOS_USER|" $INVENTORY
+sed -i "s|\${git_ssh_id_path}|$GIT_SSH_ID_PATH|" $INVENTORY
